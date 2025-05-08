c023d54368a7e9ae893cfc95b85e793a4f5f1752
Jerome Taruc <jerometaruc29@gmail.com>
2025-05-08 16:12:48 -0700

    Added scrolling to NavBar; Updated Projects width; Updated text color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,34 @@ import React from 'react'
 import '../styles/NavBar.css'
 
 const NavBar: React.FC = () => {
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, section: string) => {
+        e.preventDefault();
+        const element = document.getElementById(section);
+        const offset = 70;
+    
+        if (element) {
+            const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - offset;
+    
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    };
     return (
         <nav className="navbar">
             <h1 className="navbar-title">Jerome Taruc</h1>
             <div className="navbar-links">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#experience">Experience</a>
-                <a href="#projects">Projects</a>
+                <a href="#home" onClick={(e) => handleClick(e, 'home')}>
+                    Home
+                </a>
+                <a href="#about" onClick={(e) => handleClick(e, 'about')}>
+                    About
+                </a>
+                <a href="#projects" onClick={(e) => handleClick(e, 'projects')}>
+                    Projects
+                </a>
             </div>
             <div className="navbar-social">
                 <a 
