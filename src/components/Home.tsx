5da43913d044ce71e429c06d75ffecd0683d6dce
Jerome Taruc <jerometaruc29@gmail.com>
2025-05-09 16:13:02 -0700

    Updated style and formatting in Home.tsx/Home.css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../styles/Home.css'
+import profile_photo from '../assets/profile_photo.jpg';
 
 const Home: React.FC = () => {
     return (
-        <section id="home">
-            <h1>Hello World, I'm Jerome.</h1>
-            <h2>Welcome to V3.0 of my personal website</h2>
+        <section id="home" className="home-section">
+            <div className="home-container">
+                <div className="home-photo">
+                    <img src={profile_photo} alt="Jerome Taruc" />
+                </div>
+                <div className="home-text">
+                    <h1>Jerome Taruc</h1>
+                    <p>Computer Science student at UVic</p>
+                </div>
+            </div>
         </section>
     );
 };
